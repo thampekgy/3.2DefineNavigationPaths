d314f459fabc8112a35be08aaa0d6cd203535d0a
Peggy <pekgytham@gmail.com>
2020-01-07 01:12:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a32definenavigationpaths/GameWonFragment.kt
@@ -0,0 +1,25 @@
+package com.example.a32definenavigationpaths
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.a32definenavigationpaths.databinding.FragmentGameWonBinding
+
+
+class GameWonFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_game_won, container, false)
+
+        binding.nextMatchButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+        }
+        return binding.root
+    }
+}
